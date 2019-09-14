037d25ae2fcfa293577443c7b471a7431172a243
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-09-14 06:57:03 -0700

    finished with mvp code & working on bugs

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,15 +1,17 @@
+import { ADD_SMURF } from '../actions/action'
+
 const initialState = { 
     smurfs: [],
 }
 
-export const reducer = (state=initialState, action) => {
+export const reducer = (state = initialState, action) => {
         switch(action.type) {
             case "ADD_SMURF":
                 return { ...state, };
-            default:
-            return state;
+                default:
+                    return state;
         }
       }
       
-reducer(state, {type: "ADD_SMURF"});
+
 
