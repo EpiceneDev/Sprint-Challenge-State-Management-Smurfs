dbed2be3074911d8b347039d744100f543bce7ca
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-10-11 08:23:28 -0700

    first commit and answers completed

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,7 +4,7 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import "./index.css";
 import App from "./components/App";
-import { reducer } from './reducers/reducer'
+import { reducer } from './reducers/reducer';
 
 const store = createStore(reducer);
 
